0a36cb053eb3322261460c7f88bdc1da3ffd669f
aka-daimonas <kcutsav262@gmail.com>
2023-06-25 17:59:46 +0545

    Added decimal to binary

---
+++ dec_to_bin.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+int to_binary(int decimal_num)
+{
+ int binary_num[100];
+ int i = 0, size;
+ while (decimal_num)
+ {
+  binary_num[i] = decimal_num % 2;
+  decimal_num /= 2;
+  i++;
+ }
+ size = i;
+ for (i = size - 1; i >= size; i--)
+ {
+  printf("%d", binary_num[i]);
+ }
+ return 0;
+}
+
+int main()
+{
+ int dec_num;
+ printf("Enter a decimal number: ");
+ scanf("%d", &dec_num);
+
+ to_binary(dec_num);
+}
